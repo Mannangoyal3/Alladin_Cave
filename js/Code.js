8cbab13b7f0b53bbbc2b8645da76f70c622dfeb4
Mannangoyal3 <75422299+Mannangoyal3@users.noreply.github.com>
2022-03-17 16:13:07 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "EGG";
+const accessCode2 = "CANDLE";
+const accessCode3 = "AGE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("G E E", 100,50)
+    fill("lightblue")
+    text("Hint: What has to be broken before you can use it?", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("E N D L E C", 700,150)
+    fill("lightblue")
+    text("Hint: I am tall when I am young, and I am",700,170);
+    text("short when I am old. What am I?", 700,185)
+
+    fill("white")
+    textSize(15)
+    text("G E A", 100,250)
+    fill("lightblue")
+    text("Hint: What goes up but never comes down?", 100,270)
+
+}
